fbaef0b77ce96e345f9638beb1647cb20d2bf3bc
ARIYA TIPPAWAN <ariya.tipp@bulive.net>
2016-02-03 11:06:48 +0700

    Creating Hazard 2/02

---
+++ SpaceSample/Assets/Scripts/DrestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other)
+	{
+		//avoid destruction by our game box boundary
+		if(other.tag == "Boundary")
+		{
+			return;
+		}
+		Object.DestroyObject(other.gameObject);
+		Object.DestroyObject(gameObject);
+	}
+}﻿
