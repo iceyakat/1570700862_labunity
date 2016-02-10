1829022219e34eb1aff84476561c7bf2b8e4f293
ARIYA TIPPAWAN <ariya.tipp@bulive.net>
2016-02-10 11:06:07 +0700

    counting point and display the score 3/02

--- SpaceSample/Assets/Scripts/DestroyByContact.cs
+++ SpaceSample/Assets/Scripts/DestroyByContact.cs
@@ -5,6 +5,22 @@ public class DestroyByContact : MonoBehaviour
 {
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+	
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+	
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.tag == "Boundary")
@@ -12,9 +28,11 @@ public class DestroyByContact : MonoBehaviour
 			return;
 		}
 		Instantiate(explosion, transform.position, transform.rotation);
-		if (other.tag == "Player") {
-			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
 		}
+		gameController.AddScore (scoreValue);
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
