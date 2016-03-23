7a240a060812d7e451ad28cbbfef20234014bbcd
ARIYA TIPPAWAN <ariya.tipp@bulive.net>
2016-03-23 11:17:49 +0700

    2hours

--- SpaceSample/Assets/Scripts/PlayerController.cs
+++ SpaceSample/Assets/Scripts/PlayerController.cs
@@ -2,51 +2,48 @@
 using System.Collections;
 
 [System.Serializable]
-public class Boundary
+public class Boundary 
 {
 	public float xMin, xMax, zMin, zMax;
 }
 
-
-public class PlayerController : MonoBehaviour 
+public class PlayerController : MonoBehaviour
 {
-	
 	public float speed;
 	public float tilt;
-	public Boundary boundary;
-	
+	public Done_Boundary boundary;
+
 	public GameObject shot;
-	public GameObject shotSpawn; 
+	public Transform shotSpawn;
 	public float fireRate;
-	
-	private float nextFire; 
-	
+
+	private float nextFire;
+
 	void Update ()
 	{
-		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
 			nextFire = Time.time + fireRate;
-			Instantiate (shot, shotSpawn.transform.position, shotSpawn.transform.rotation);
+			Instantiate(shot, shotSpawn.transform.position, shotSpawn.transform.rotation);
 			GetComponent<AudioSource>().Play ();
 		}
 	}
+
 	void FixedUpdate ()
 	{
-		
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		
+
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement * speed;
-		
+
 		GetComponent<Rigidbody>().position = new Vector3
 			(
-				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-				);
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			);
+
 		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
-	
 }
-
-
-
-
